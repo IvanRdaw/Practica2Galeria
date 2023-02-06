5dde20037dc33fdc189b23b0e905a5eb0d4f0187
IvanRdaw <ivanr.daw@gmail.com>
2023-02-06 19:03:48 +0100

    creando proyecto con pom y .gitignore

---
+++ src/main/java/com/programacion2eva/App.java
@@ -0,0 +1,13 @@
+package com.programacion2eva;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
